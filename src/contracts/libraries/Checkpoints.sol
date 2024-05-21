c3e5eafeb126e2d96f4e954db8b50788eaf7732e
Andrey Korokhov <a.korokhov@statemind.io>
2024-05-21 13:23:54 +0400

    checkpoints  docstrings

--- src/contracts/libraries/Checkpoints.sol
+++ src/contracts/libraries/Checkpoints.sol
@@ -3,6 +3,10 @@ pragma solidity 0.8.25;
 
 import {Checkpoints as OZCheckpoints} from "@openzeppelin/contracts/utils/structs/Checkpoints.sol";
 
+/**
+ * @dev This library defines the `Trace*` struct, for checkpointing values as they change at different points in
+ * time, and later looking up past values by key.s
+ */
 library Checkpoints {
     using OZCheckpoints for OZCheckpoints.Trace208;
 
@@ -25,14 +29,30 @@ library Checkpoints {
         uint256 _value;
     }
 
+    /**
+     * @dev Pushes a (`key`, `value`) pair into a Trace208 so that it is stored as the checkpoint.
+     *
+     * Returns previous value and new value.
+     */
     function push(Trace208 storage self, uint48 key, uint208 value) internal returns (uint208, uint208) {
         return self._trace.push(key, value);
     }
 
+    /**
+     * @dev Returns the value in the last (most recent) checkpoint with key lower or equal than the search key, or zero
+     * if there is none.
+     */
     function upperLookupRecent(Trace208 storage self, uint48 key) internal view returns (uint208) {
         return self._trace.upperLookupRecent(key);
     }
 
+    /**
+     * @dev Returns the value in the last (most recent) checkpoint with key lower or equal than the search key, or zero
+     * if there is none.
+     *
+     * NOTE: This is a variant of {upperLookupRecent} that can be optimized by getting the hint
+     * (index of the checkpoint with key lower or equal than the search key).
+     */
     function upperLookupRecent(Trace208 storage self, uint48 key, uint32 hint) internal view returns (uint208) {
         Checkpoint208 memory hintCheckpoint = at(self, hint);
         if (hintCheckpoint._key == key) {
@@ -46,22 +66,36 @@ library Checkpoints {
         return upperLookupRecent(self, key);
     }
 
+    /**
+     * @dev Returns the value in the most recent checkpoint, or zero if there are no checkpoints.
+     */
     function latest(Trace208 storage self) internal view returns (uint208) {
         return self._trace.latest();
     }
 
+    /**
+     * @dev Returns the number of checkpoint.
+     */
     function length(Trace208 storage self) internal view returns (uint256) {
         return self._trace.length();
     }
 
+    /**
+     * @dev Returns checkpoint at given position.
+     */
     function at(Trace208 storage self, uint32 pos) internal view returns (Checkpoint208 memory) {
         OZCheckpoints.Checkpoint208 memory checkpoint = self._trace.at(pos);
         return Checkpoint208({_key: checkpoint._key, _value: checkpoint._value});
     }
 
+    /**
+     * @dev Pushes a (`key`, `value`) pair into a Trace256 so that it is stored as the checkpoint.
+     *
+     * Returns previous value and new value.
+     */
     function push(Trace256 storage self, uint48 key, uint256 value) internal returns (uint256, uint256) {
         if (self._values.length == 0) {
-            self._trace.push(0, uint208(0));
+            self._trace.push(0, 0);
             self._values.push(0);
         }
 
@@ -72,6 +106,10 @@ library Checkpoints {
         return (self._values[len - 1], value);
     }
 
+    /**
+     * @dev Returns the value in the last (most recent) checkpoint with key lower or equal than the search key, or zero
+     * if there is none.
+     */
     function upperLookupRecent(Trace256 storage self, uint48 key) internal view returns (uint256) {
         uint208 idx = self._trace.upperLookupRecent(key);
         if (idx == 0) {
@@ -80,6 +118,13 @@ library Checkpoints {
         return self._values[idx];
     }
 
+    /**
+     * @dev Returns the value in the last (most recent) checkpoint with key lower or equal than the search key, or zero
+     * if there is none.
+     *
+     * NOTE: This is a variant of {upperLookupRecent} that can be optimized by getting the hint
+     * (index of the checkpoint with key lower or equal than the search key).
+     */
     function upperLookupRecent(Trace256 storage self, uint48 key, uint32 hint) internal view returns (uint256) {
         Checkpoint256 memory hintCheckpoint = at(self, hint);
         if (hintCheckpoint._key == key) {
@@ -93,18 +138,28 @@ library Checkpoints {
         return upperLookupRecent(self, key);
     }
 
+    /**
+     * @dev Returns the value in the most recent checkpoint, or zero if there are no checkpoints.
+     */
     function latest(Trace256 storage self) internal view returns (uint256) {
         uint208 idx = self._trace.latest();
         if (idx == 0) {
             return 0;
         }
-        return self._values[self._trace.latest()];
+        return self._values[idx];
     }
 
+    /**
+     * @dev Returns the number of checkpoint.
+     */
     function length(Trace256 storage self) internal view returns (uint256) {
-        return self._trace.length() == 0 ? 0 : self._trace.length() - 1;
+        uint256 len = self._trace.length();
+        return len == 0 ? 0 : len - 1;
     }
 
+    /**
+     * @dev Returns checkpoint at given position.
+     */
     function at(Trace256 storage self, uint32 pos) internal view returns (Checkpoint256 memory) {
         OZCheckpoints.Checkpoint208 memory checkpoint = self._trace.at(pos + 1);
         return Checkpoint256({_key: checkpoint._key, _value: self._values[checkpoint._value]});
