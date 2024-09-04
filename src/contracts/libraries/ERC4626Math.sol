6e38e39242842e6b5ec22c0805ad5e5681803125
Kresh <andrey@symbiotic.fi>
2024-09-04 13:26:31 +0400

    chore: interfaces and libraries accessible from lower versions

--- src/contracts/libraries/ERC4626Math.sol
+++ src/contracts/libraries/ERC4626Math.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.0;
 
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
--- src/interfaces/common/IMigratableEntityProxy.sol
+++ src/interfaces/common/IMigratableEntityProxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.0;
 
 import {IERC1967} from "@openzeppelin/contracts/interfaces/IERC1967.sol";
 
