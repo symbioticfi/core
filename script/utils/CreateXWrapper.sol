67bddb907ca6e5208b4195b7919c126b8b5cf915
Sergii Liakh <liakh@ukr.net>
2025-09-26 09:23:15 +0100

    feat: added ICreateX interface

--- script/utils/CreateXWrapper.sol
+++ script/utils/CreateXWrapper.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ICreateX} from "@createx/contracts/ICreateX.sol";
+import {ICreateX} from "./interfaces/ICreateX.sol";
 
 /**
  * @title CreateXWrapper
