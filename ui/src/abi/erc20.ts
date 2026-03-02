61f0a17e9a02a7bd7a97cbfdc18bd821cd74a6ea
Andrey <andrey@symbiotic.fi>
2026-03-02 20:46:59 +0400

    chore: clean

--- ui/src/abi/erc20.ts
+++
@@ -1,9 +0,0 @@
-export const erc20Abi = [
-  {
-    type: "function",
-    name: "decimals",
-    stateMutability: "view",
-    inputs: [],
-    outputs: [{ name: "decimals", type: "uint8" }],
-  },
-] as const;
